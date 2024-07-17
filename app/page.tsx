88a0dff2271385e3e22cc48b46d0645782318cbf
KiwiPetal <kiwipetal@kittymail.com>
2024-07-17 22:54:31 +0200

    Removing placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen p-24 gap-4">
     <NewList />
-    <p>
-      List View
-    </p>
     </main>
   );
 }
