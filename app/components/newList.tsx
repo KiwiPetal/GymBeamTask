efe98af0869485a472248439f7a96f99d0e6d5c7
KiwiPetal <kiwipetal@kittymail.com>
2024-07-18 18:20:58 +0200

    Adding Dark mode

--- app/components/newList.tsx
+++ app/components/newList.tsx
@@ -62,6 +62,8 @@ export default function NewList() {
     axios.post("https://66953a884bd61d8314ca986c.mockapi.io/api/lists", {
       name: listName,
       items: itemList
+    }).then(() => {
+      window.location.reload();
     });
 
     setItemList([]);
@@ -126,15 +128,6 @@ export default function NewList() {
   // }}}
 
   // Framer motion variants {{{
-  const containerVariants = {
-    hidden: {
-      backgroundColor: "transparent"
-    },
-    visible: {
-      backgroundColor: "#1c1917"
-    }
-  }
-
   const buttonVariants = {
     hidden: {
       pointerEvents: "none",
@@ -177,11 +170,8 @@ export default function NewList() {
   // Render {{{
   return (
     <AnimatePresence>
-      <motion.div
-        variants={containerVariants}
-        initial="hidden"
-        animate={listName === "" ? "hidden" : "visible"}
-        className={`flex flex-col bg-stone-900 p-3 rounded items-center container gap-2 lg:w-64 overflow-hidden`}>
+      <div
+        className={`${listName === "" ? "bg-transparent" : "bg-stone-100 border-2 border-stone-300 shadow-xl dark:border-none dark:bg-stone-900"} flex flex-col text-black dark:text-white p-3 rounded items-center container gap-2 md:w-64 overflow-hidden`}>
 
 
         { /* TODO auto expand? */}
@@ -189,17 +179,17 @@ export default function NewList() {
           placeholder="New List Name..."
           value={listName}
           onChange={(e) => handleNameChange(e.target.value)}
-          className="block w-full p-2 overflow-hidden text-xl font-bold rounded outline-none bg-stone-800 placeholder:text-stone-500 focus:outline-stone-500" />
+          className="block w-full p-2 overflow-hidden text-xl font-bold rounded outline-none bg-stone-200 dark:bg-stone-800 placeholder:text-stone-500 focus:outline-stone-500" />
         {
           itemList.map((item, i) => {
             return (
-              <div key={i} className="flex flex-col w-full py-2 border-b-2 border-stone-800 gap-1">
+              <div key={i} className="flex flex-col w-full py-2 border-b-2 border-stone-300 dark:border-stone-800 gap-1">
                 <div className={`flex relative flex-row justify-center items-center gap-1`}>
                   <input
                     type="checkbox"
                     checked={itemList[i].completed}
                     onChange={() => updateItem({id: item.id, header: "completed", value: !itemList[i].completed})}
-                    className="h-4 border-[1px] border-pink-900 rounded appearance-none cursor-pointer aspect-square after:content-[''] after:rounded after:absolute after:bg-pink-600 after:h-4 after:opacity-0 checked:after:opacity-100 after:transition-all after:duration-200 after:aspect-square" />
+                    className="h-4 border-[1px] border-pink-900 rounded appearance-none cursor-pointer aspect-square after:content-[''] after:rounded after:absolute after:bg-pink-600 after:-translate-y-[1px] after:-translate-x-[1px] after:h-4 after:opacity-0 checked:after:opacity-100 after:transition-all after:duration-200 after:aspect-square" />
                     {itemList[i].props.priority && (
                       <p className="ml-1 text-xl text-red-700">
                       !
@@ -211,21 +201,21 @@ export default function NewList() {
                     autoFocus
                     onChange={(e) => updateItem({id: item.id, header: "name", value: e.target.value})}
                     className="block w-full h-8 p-2 bg-transparent outline-none peer placeholder:text-stone-500 focus:outline-none" />
-                  <div className="w-0 peer-focus:w-full absolute bottom-0 left-0 h-[2px] duration-200 bg-stone-500" />
-                  <div className="relative flex items-center h-6 p-1 rounded-full cursor-pointer group hover:bg-transparent bg-stone-700 aspect-square transition-all duration-150">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} className="size-6 stroke-white group-hover:stroke-none transition-all duration-150">
+                  <div className="w-0 peer-focus:w-full absolute bottom-0 left-0 h-[2px] duration-200 bg-stone-400 dark:bg-stone-500" />
+                  <div className="relative flex items-center h-6 p-1 rounded-full cursor-pointer group hover:bg-transparent dark:hover:bg-transparent bg-stone-200 dark:bg-stone-700 aspect-square transition-all duration-150">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} className="size-6 stroke-black dark:stroke-white group-hover:stroke-none transition-all duration-150">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                     <div className="absolute right-0 z-10 flex flex-col gap-4 top-1/2 -translate-y-1/2 group-hover:scale-100 scale-0 transition-all duration-150 origin-right">
                       <p
                         onClick={() => setOpenItems((prev) => ({...prev, [itemList[i].id]: !openItems[itemList[i].id]}))}
-                        className="p-1 px-2 text-lg text-center rounded shadow-md active:scale-90 bg-cyan-800">
+                        className="p-1 px-2 text-lg text-center bg-blue-300 rounded shadow-md active:scale-90 dark:bg-cyan-800">
                         Properties
                       </p>
                       <p
                         onClick={() => deleteItem(item)}
-                        className="p-1 px-2 text-lg text-center bg-red-800 rounded shadow-xl active:scale-90">
+                        className="p-1 px-2 text-lg text-center bg-red-300 rounded shadow-xl dark:bg-red-800 active:scale-90">
                         Delete
                       </p>
                     </div>
@@ -246,7 +236,7 @@ export default function NewList() {
                   {itemList[i].props.tags?.map((tag, _i) => {
                     return (
                       <div
-                        className="relative p-1 rounded group bg-stone-800 text-stone-500"
+                        className="relative p-1 border-2 rounded group bg-stone-200 border-stone-300 dark:border-none dark:bg-stone-800 text-stone-500"
                         key={"tag" + _i}>
                         #{tag}
                         <div
@@ -273,7 +263,7 @@ export default function NewList() {
                     type="checkbox"
                     checked={itemList[i].props.priority}
                     onChange={() => updateProps({id: itemList[i].id, header:"priority", value: !itemList[i].props.priority})}
-                    className="h-4 border-[1px] border-red-900 rounded appearance-none cursor-pointer aspect-square after:content-[''] after:rounded after:absolute after:bg-red-600 after:h-4 after:opacity-0 checked:after:opacity-100 after:transition-all after:duration-200 after:aspect-square" />
+                    className="h-4 border-[1px] border-red-500 rounded appearance-none cursor-pointer aspect-square after:content-[''] after:rounded after:absolute after:bg-red-500 after:h-4 after:opacity-0 checked:after:opacity-100 after:transition-all after:duration-200 after:aspect-square" />
                   </label>
                   <label>
                     Details
@@ -281,7 +271,7 @@ export default function NewList() {
                       value={itemList[i].props.details}
                       placeholder="Additional details..."
                       onChange={(e) => updateProps({id: itemList[i].id, header: "details", value: e.target.value})}
-                      className="block w-full h-8 p-2 rounded outline-none bg-stone-800 peer placeholder:text-stone-500 focus:outline-none" />
+                      className="block w-full h-8 p-2 rounded outline-none bg-stone-200 dark:bg-stone-800 peer placeholder:text-stone-500 focus:outline-none" />
                   </label>
                   <label>
                     Tags
@@ -289,19 +279,19 @@ export default function NewList() {
                       value={tagsInput[itemList[i].id]}
                       placeholder="Add multiple tags..."
                       onChange={(e) => tagsEdit(e.target.value, itemList[i].id)}
-                      className="block w-full h-8 p-2 rounded outline-none bg-stone-800 peer placeholder:text-stone-500 focus:outline-none" />
+                      className="block w-full h-8 p-2 rounded outline-none bg-stone-200 dark:bg-stone-800 peer placeholder:text-stone-500 focus:outline-none" />
                   </label>
                   <label>
                     Due date
                     <input
                       value={itemList[i].props.date}
                       onChange={(e) => updateProps({id: itemList[i].id, header: "date", value: e.target.value})}
-                      className="block w-full h-8 p-2 rounded outline-none bg-stone-800 peer placeholder:text-stone-500 focus:outline-none"
+                      className="block w-full h-8 p-2 rounded outline-none bg-stone-200 dark:bg-stone-800 peer placeholder:text-stone-500 focus:outline-none"
                       type="date" />
                   </label>
                   <button
                     onClick={() => confirmProps(itemList[i].id)}
-                    className="w-full p-1 mt-2 bg-green-700 rounded">
+                    className="w-full p-1 mt-2 bg-green-300 rounded dark:bg-green-700">
                     Confirm properties
                   </button>
                 </motion.div>
@@ -322,11 +312,11 @@ export default function NewList() {
             className="block w-full h-8 p-2 bg-transparent rounded-l-full outline-none cursor-pointer placeholder:text-stone-500" />
           <button
             onClick={() => handleSend()}
-            className={`appearance-none overflow-hidden cursor-pointer active:scale-90 w-full text-center p-2 rounded bg-pink-900`}>
+            className={`appearance-none overflow-hidden cursor-pointer active:scale-90 w-full text-center p-2 rounded bg-pink-300 dark:bg-pink-900`}>
             Add new list
           </button>
         </motion.div>
-      </motion.div>
+      </div>
     </AnimatePresence>
   );
   // }}}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Config from "./components/config";
 import NewList from "./components/newList"
+import ReadLists from "./components/readLists";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center min-h-screen p-24 gap-4">
-    <NewList />
+    <main className="flex flex-col items-center min-h-screen p-6 bg-gradient-to-b from-white to-stone-200 dark:from-black dark:to-stone-950 gap-4">
+      <Config />
+      <NewList />
+      <ReadLists />
     </main>
   );
 }
